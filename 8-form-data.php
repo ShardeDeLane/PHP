3c90cdd99ba9e450d766b8b773de1e848d578ef0
Sharde DeLane <rhcprocker@yahoo.com>
2020-07-28 03:02:55 -0500

    Update 8-form-data.php

--- 8-form-data.php
+++ 8-form-data.php
@@ -1,4 +1,21 @@
 <?php 
+
+// POST - the data is sent to the server as a package in a separate communication with the processing script
+// data sent through POST will not be visible in the url
+
+// POST - Advantages & Disadvantages
+// more secure than GET because user-entered information is not visible in the url query string or in the server logs
+// larger limit on amount of data that can be passed and can send text data as well as 
+// binary data (uploading a file) using POST
+// since data is not visible in the url, it's not possible to bookmark a page with the specific query
+
+// PHP provides super global variable $_POST to access information sent via post method
+// or submitted through an html form using the method="post"
+
+// REQUEST variable
+// php provides another superglobal variable $_REQUEST that contains the values of 
+// $_GET and $_POST variables as well as the values of the $_COOKIE superglobal variable
+
 // Super Globals GET and POST
 // form_process.php
 if(isset($_POST["submit"])) {
